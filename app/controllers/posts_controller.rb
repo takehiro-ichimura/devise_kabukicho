ccf83173cc6d26f317a538d5b7c68a762f23df8d
TK201_DTB <sprollhrmk@gmail.com>
2020-04-16 20:23:55 +0900

    first

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, {only: [:new, :create, :show, :edit, :update, :destroy]}
+  before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
+  def ensure_correct_user
+    if current_user.id != params[:id].to_i
+      flash[:notice] = "この操作をする権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
+  def index
+    @posts = Post.all
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(
+      content: params[:content],
+      user_id: current_user.id
+    )
+    if @post.save
+      flash[:notice] = "投稿成功"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
+  end
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
+  end
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "更新成功"
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  get 'posts/index'
+  get 'users/index'
+  devise_for :users
+  root 'home#top'
+  get '/' => "home#top"
+  get '/about' => "home#about"
+  get "/users/index" => "users#index"
+  get "users/:id" => "users#show"
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+  get "posts/:id" => "posts#show"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
