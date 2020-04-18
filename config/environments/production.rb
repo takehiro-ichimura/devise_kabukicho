21984fa171db91e5f8fab3881c1dd7896c140370
TK201_DTB <sprollhrmk@gmail.com>
2020-04-18 12:58:06 +0900

    for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'http://kabukicho-sns.herokuapp.com' }
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {host: 'http://kabukicho2.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :user_name => ENV['SENDGRID_USERNAME'],
